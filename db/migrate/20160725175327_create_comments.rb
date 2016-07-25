5b1697708399c29190f6570480bbb0afe1becfbc
Emily Schmidtling <eyschmidtling@gmail.com>
2016-07-25 11:56:05 -0700

    adds Comment model, welcome controller & view, posts controller & view, and posts seed data

---
+++ db/migrate/20160725175327_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.references :post, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+      t.text :content
+      t.string :photo_url
+
+      t.timestamps null: false
+    end
+  end
+end
