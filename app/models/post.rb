1585c53ceccc7cc385eb6677da443b3cb6d6e8c6
Emily Schmidtling <eyschmidtling@gmail.com>
2016-07-26 11:53:51 -0700

    adds nickname validation for new user setup

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+
+  # validates :user_id, presence: true
+
   belongs_to :user
     # accepts_nested_attributes_for :user
   has_many :comments
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_uniqueness_of :nickname
+  validates_presence_of :nickname
+
   has_many :posts
   has_many :comments
 
