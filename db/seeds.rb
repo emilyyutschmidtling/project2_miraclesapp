17ff032b065dfcc9ddd838e28e11397beb6a8ebb
Emily Schmidtling <eyschmidtling@gmail.com>
2016-07-29 03:00:15 -0700

    adds image display if post or comment includes url for an image

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,17 @@
 #     photo_url: "http://t3.gstatic.com/images?q=tbn:ANd9GcQu6t289OZIOlPWU_AkeaL-3-kb2AywKUACnXSer1g_-pcpi0mi"
 #   }
 # ])
+
+create_table "posts", force: :cascade do |t|
+  t.integer  "user_id"
+  t.text     "content"
+  t.string   "photo_url"
+  t.datetime "created_at", null: false
+  t.datetime "updated_at", null: false
+end
+
+user1 = User.create id: 1, email: "hpotter@hogwarts.edu", nickname: "The Chosen One", first_name: "Harry", last_name: "Potter"
+
+user2 = User.create id: 2, email: "rweasley@hogwarts.edu", nickname: "Won Won", first_name: "Ron", last_name: "Weasley"
+
+post1 = Post.create id: 1, user_id: 2, content: "No extra potions homework today!"
